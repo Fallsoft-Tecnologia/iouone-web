82035f70a98044176d11d6583ddbdc31a2be196d
Gabriel Andrade Almeida <59920810+g-andradd@users.noreply.github.com>
2024-11-08 00:13:34 -0300

    refactor: component treino responsivo

--- src/app/modules/views/treino/treino.component.ts
+++ src/app/modules/views/treino/treino.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./treino.component.css']
 })
 export class TreinoComponent {
-
+  treinos = [
+    {
+      title: 'Leg Press',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting 
+      industry. Lorem Ipsum has been the industry's standard dummy text ever 
+      since the 1500s, when an unknown printer took a galley of type and 
+      scrambled it to make a type specimen book.`,
+      imageSrc: '../../../../assets/treino/leg-press.png'
+    },
+    {
+      title: 'Agachamento',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting 
+      industry. Lorem Ipsum has been the industry's standard dummy text ever 
+      since the 1500s, when an unknown printer took a galley of type and 
+      scrambled it to make a type specimen book.`,
+      imageSrc: '../../../../assets/treino/agachamento.png'
+    },
+    {
+      title: 'Rosca',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting 
+      industry. Lorem Ipsum has been the industry's standard dummy text ever 
+      since the 1500s, when an unknown printer took a galley of type and 
+      scrambled it to make a type specimen book.`,
+      imageSrc: '../../../../assets/treino/rosca.png'
+    },
+    {
+      title: 'Leg Press',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting 
+      industry. Lorem Ipsum has been the industry's standard dummy text ever 
+      since the 1500s, when an unknown printer took a galley of type and 
+      scrambled it to make a type specimen book.`,
+      imageSrc: '../../../../assets/treino/leg-press.png'
+    },
+    {
+      title: 'Agachamento',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting 
+      industry. Lorem Ipsum has been the industry's standard dummy text ever 
+      since the 1500s, when an unknown printer took a galley of type and 
+      scrambled it to make a type specimen book.`,
+      imageSrc: '../../../../assets/treino/agachamento.png'
+    },
+    {
+      title: 'Rosca',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting 
+      industry. Lorem Ipsum has been the industry's standard dummy text ever 
+      since the 1500s, when an unknown printer took a galley of type and 
+      scrambled it to make a type specimen book.`,
+      imageSrc: '../../../../assets/treino/rosca.png'
+    }
+  ];
 }
