9ee146ba9c7763003cce2a898c22154e0e8ab190
Gabriel Andrade Almeida <59920810+g-andradd@users.noreply.github.com>
2025-02-09 16:09:19 -0300

    fix: router get

--- src/app/shared/components/receita-completa/receita-completa-routing.module.ts
+++ src/app/shared/components/receita-completa/receita-completa-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { dietaResolver } from '../../resolver/dieta.resolver';
 import { ReceitaCompletaComponent } from './receita-completa.component';
 
 const routes: Routes = [
   {
     path: ':id',
     component: ReceitaCompletaComponent,
-    resolve: {
-        dietas: dietaResolver,
-      },
   }
 ];
 
