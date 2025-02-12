6ab68331cb97c00bb7276375cb3ddb6ea82cc65c
Daniel Mineiro <daniel.mineiro10@gmail.com>
2025-02-12 19:51:08 -0300

    fix - > Ajuste tela home

--- src/app/modules/views/home/home.component.ts
+++ src/app/modules/views/home/home.component.ts
@@ -20,48 +20,34 @@ export class HomeComponent implements OnInit {
 
   loadPosts(page: number = 0, size: number = 10): void {
     this.postService.getPosts(page, size).subscribe(response => {
-      console.log(response);
       this.posts = response.content.map((post: Post) => ({
         id: post.id,
         nomePessoa: post.nomePessoa,
         mensagem: post.mensagem,
-        comments: [],
+        // Processando o comentário para adicionar o nome do cliente
+        comentariov2DTO: post.comentariov2DTO.map((comment: any) => ({
+          id: comment.id,
+          nomeCliente: comment.pessoa?.nome,  // Extraindo o nome da pessoa dentro de cada comentário
+          mensagemComentario: comment.mensagemComentario
+        })),
         newCommentContent: ''
-
-        
       }));
       console.log(this.posts);
-      this.loadCommentsForPosts();
+      console.log(response);
     });
   }
-
-  loadCommentsForPosts(): void {
-    this.posts.forEach(post => {
-      this.postService.getComments(post.id).subscribe(response => {
-        console.log('Resposta dos comentários:', response); // Adicionando log para ver a resposta
-        if (response && response.content) {
-          post.comments = response.content.map((comment: Comment) => ({
-            id: comment.id,
-            nomeCliente: comment.nomeCliente,
-            mensagemComentario: comment.mensagemComentario
-          }));
-        } else {
-          console.log(`Nenhum comentário encontrado para o post ${post.id}`);
-          post.comments = []; // Garantir que o campo de comentários seja um array vazio se não houver dados
-        }
-      });
-    });
-  }
+  
   
 
   addPost(): void {
     if (this.newPostContent.trim()) {
       this.postService.createPost(this.newPostContent).subscribe(response => {
+        console.log(response);
         this.posts.unshift({
           id: response.id,
-          nomePessoa: response.user,
+          nomePessoa: response.pessoa.nome,
           mensagem: response.mensagem,
-          comments: [],
+          comentariov2DTO: [],
           newCommentContent: ''
         });
         this.newPostContent = '';
@@ -73,9 +59,9 @@ export class HomeComponent implements OnInit {
     const post = this.posts.find(p => p.id === postId);
     if (post && post.newCommentContent.trim()) {
       this.postService.createComment(postId, post.newCommentContent).subscribe(response => {
-        post.comments.push({
+        post.comentariov2DTO.push({
           id: response.id,
-          nomeCliente: response.user,
+          nomeCliente: response.nomeCliente,
           mensagemComentario: response.mensagemComentario
         });
         post.newCommentContent = '';
